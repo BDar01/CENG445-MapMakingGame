5849fbda8d00e4e29b135f9b17154ad79d3e3985
BDar01 <111367260+BDar01@users.noreply.github.com>
2023-11-23 23:53:26 +0300

    Updated test.py to handle 2 players on same map

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ freezerObject2 = Freezer("Freezer2", 3, 6, 500)
 config1 = {"image":"BackgroundImage1.jpg", 
            "playervision": 10, 
            "playerh": 100, 
-           "playerrepo": [("Mine", "Mine Repo1", 3, 20, 1000), ("Freezer", "Freezer Repo1", 3, 5, 10000)], 
+           "playerrepo": [("Mine", "Mine Repo1", 3, 20, 1000), ("Freezer", "Freezer Repo1", 1, 5, 10000)], 
            "objects": [(2, 3, mineObject), (10, 10, freezerObject)]
         }
 size1 = (612, 408)
@@ -38,17 +38,34 @@ print("Baran joined")
 print("Map 1 Line 2:", map1) #Map 1 Line 2, Here we can see that Player 1 is added to the objects of the map 1, which means the player properly joined the map.
 print(separator)
 
+player1b = map1.join("Dar", "Red Team")
+print("Dar joined")
+print("Map 1 Line 3:", map1) #Map 1 Line 3, Here we can see that Player 1b is added to the objects of the map 1, which means the player properly joined the map.
+print(separator)
+
 player2 = map2.join("Basim", "Blue Team")
 print("Basim joined")
 print("Map 2 Line 2:", map2) #Map 2 Line 2, Here we can see that Player 2 is added to the objects of the map 2, which means the player properly joined the map.
 print(separator)
 
-print("Map 1 Line 3:", player1)
+print("Map 1 Line 4:", player1)
+print(separator)
+print("Map 1 Line 5:", player1b)
 print(separator)
 player1.drop("Mine")
 print(map1)
 print(player1.map.teams[player1.team])
-print("Map 1 Line 4:", player1) #By looking to this line (comparing it with Map 1 Line 3) we can see it dropped the Mine and since the mine is in our posiiton and we didn't go away before it activates, it decreased our health value.
+print("Map 1 Line 6:", player1) #By looking to Map 1 line 6 and 7 (comparing it with Map 1 Line 4 and 5) we can see it dropped the Mine and since the mine is in our posiiton and we didn't go away before it activates, it decreased our health value.
+print(separator)
+print("Map 1 Line 7:", player1b)
+print(separator)
+
+player1b.drop("Mine")
+print(map1)
+print(player1b.map.teams[player1b.team])
+print("Map 1 Line 8:", player1) #By looking to Map 1 line 8 and 9 (comparing it with Map 1 Line 6 and 7) we can see it dropped the Mine and since the mine is in our posiiton and we didn't go away before it activates, it decreased our health value.
+print(separator)
+print("Map 1 Line 9", player1b)
 print(separator)
 
 print("Map 2 Line 3:", player2)
@@ -60,9 +77,9 @@ print("Map 2 Line 4:", player2) #By looking to this line (comparing it with Map 
 
 
 player1.move("E")
-print("Map 1 Line 5:", "Global View Update", ([obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+print("Map 1 Line 10:", "Global View Update", ([obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
         [obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the global map has been affected.
-print("Map 1 Line 6:", "Local View Update", ([obj for obj in player1.map.teams[player1.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+print("Map 1 Line 11:", "Local View Update", ([obj for obj in player1.map.teams[player1.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
         [obj for obj in player1.map.teams[player1.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the local map has been affected.
 player1.move("W")
 player1.move("NE")
@@ -72,15 +89,17 @@ player1.move("S")
 player1.move("N")
 player1.move("SE") 
 print(separator)
-print("Map 1 Line 6:", ([obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+print("Map 1 Line 12:", ([obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
         [obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #Expected to be in (2,0)
 player1.move("S") #Expected to not change. (checking bounds)
 print(separator)
-print("Map 1 Line 7:", ([obj for obj in map1.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+print("Map 1 Line 13:", ([obj for obj in map1.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
         [obj for obj in map1.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #Expected to be in (2,0)
 
 player1.drop("Freezer")
-print("Map 1 Line 8:", player1) #Freezer dropped, freezed for a time interval, since we didn't move away before it gets activated.
+print("Map 1 Line 14", player1b) #Not affected by Freezer dropped by Player 1, as Player 1b is out of range
+print(separator)
+print("Map 1 Line 15:", player1) #Freezer dropped, freezed for a time interval, since we didn't move away before it gets activated.
 print(separator)
 
 player2.move("E")
