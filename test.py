f566d60d047da0aadee418b9fa5e714e58859395
BDar01 <111367260+BDar01@users.noreply.github.com>
2023-11-23 23:24:15 +0300

    Updated test.py to handle testcase of 2 maps (Still needs to be extended for Phase 1 demo)

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ from game import *
 
 separator = "****************"
 
-mineObject = Mine("Mine1", 7, 30, 1000)    
+mineObject = Mine("Mine1", 7, 30, 1000) 
+mineObject2 = Mine("Mine2", 10, 50, 800) 
 #healthObject = Health("Health1", 20, False) If we add this object, since we didn't implement the threads functionality, it will run forever and the program won't work.
 #healthObject2 = Health("Health2", 20, True) If we add this object, since we didn't implement the threads functionality, it will run forever and the program won't work.
 freezerObject = Freezer("Freezer1", 5, 4, 1000)
+freezerObject2 = Freezer("Freezer2", 3, 6, 500)
 
 config1 = {"image":"BackgroundImage1.jpg", 
            "playervision": 10, 
@@ -13,48 +15,96 @@ config1 = {"image":"BackgroundImage1.jpg",
            "playerrepo": [("Mine", "Mine Repo1", 3, 20, 1000), ("Freezer", "Freezer Repo1", 3, 5, 10000)], 
            "objects": [(2, 3, mineObject), (10, 10, freezerObject)]
         }
-size = (612, 408)
-map1 = Map("Map1", size, config1)
-print("Map Created. Here it is:")
-print("Line 1:", map1) #Line 1, Here we can see that map instantiated properly.
+size1 = (612, 408)
+map1 = Map("Map1", size1, config1)
+print("Map 1 Created. Here it is:")
+print("Map 1 Line 1:", map1) #Map 1 Line 1, Here we can see that map 1 is instantiated properly.
 print(separator)
 
-player = map1.join("Baran", "Red Team")
+config2 = {"image":"BackgroundImage2.jpg", 
+           "playervision": 8, 
+           "playerh": 120, 
+           "playerrepo": [("Mine", "Mine Repo2", 6, 25, 1000), ("Freezer", "Freezer Repo2", 10, 3, 5000)], 
+           "objects": [(2, 3, mineObject2), (10, 10, freezerObject2)]
+        }
+size2 = (256, 256)
+map2 = Map("Map2", size2, config2)
+print("Map 2 Created. Here it is:")
+print("Map 2 Line 1:", map2) #Map 2 Line 1, Here we can see that map 2 is instantiated properly.
+print(separator)
+
+player1 = map1.join("Baran", "Red Team")
 print("Baran joined")
-print("Line 2:", map1) #Line 2, Here we can see that Player is added to the objects of the map, which means the player properly joined the map.
+print("Map 1 Line 2:", map1) #Map 1 Line 2, Here we can see that Player 1 is added to the objects of the map 1, which means the player properly joined the map.
 print(separator)
 
-print("Line 3:", player)
+player2 = map2.join("Basim", "Blue Team")
+print("Basim joined")
+print("Map 2 Line 2:", map2) #Map 2 Line 2, Here we can see that Player 2 is added to the objects of the map 2, which means the player properly joined the map.
 print(separator)
-player.drop("Mine")
+
+print("Map 1 Line 3:", player1)
+print(separator)
+player1.drop("Mine")
 print(map1)
-print(player.map.teams[player.team])
-print("Line 4:", player) #By looking to this line (comparing it with Line 3) we can see it dropped the Mine and since the mine is in our posiiton and we didn't go away before it activates, it decreased our health value.ü
-
-player.move("E")
-print("Line 5:", "Global View Update", ([obj for obj in player.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][0], 
-        [obj for obj in player.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the global map has been affected.
-print("Line 6:", "Local View Update", ([obj for obj in player.map.teams[player.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][0], 
-        [obj for obj in player.map.teams[player.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the local map has been affected.
-player.move("W")
-player.move("NE")
-player.move("NE")
-player.move("SW")
-player.move("S")
-player.move("N")
-player.move("SE") 
+print(player1.map.teams[player1.team])
+print("Map 1 Line 4:", player1) #By looking to this line (comparing it with Map 1 Line 3) we can see it dropped the Mine and since the mine is in our posiiton and we didn't go away before it activates, it decreased our health value.
 print(separator)
-print("Line 6:", ([obj for obj in player.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][0], 
-        [obj for obj in player.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][1])) #Expected to be in (2,0)
-player.move("S") #Expected to not change. (checking bounds)
-print(separator)
-print("Line 7:", ([obj for obj in map1.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][0], 
-        [obj for obj in map1.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player.id][0][1])) #Expected to be in (2,0)
 
-player.drop("Freezer")
-print("Line 8:", player) #Freezer dropped, freezed for a time interval, since we didn't move away before it gets activated.
+print("Map 2 Line 3:", player2)
+print(separator)
+player2.drop("Mine")
+print(map2)
+print(player2.map.teams[player2.team])
+print("Map 2 Line 4:", player2) #By looking to this line (comparing it with Map 2 Line 3) we can see it dropped the Mine and since the mine is in our posiiton and we didn't go away before it activates, it decreased our health value.
+
+
+player1.move("E")
+print("Map 1 Line 5:", "Global View Update", ([obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+        [obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the global map has been affected.
+print("Map 1 Line 6:", "Local View Update", ([obj for obj in player1.map.teams[player1.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+        [obj for obj in player1.map.teams[player1.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the local map has been affected.
+player1.move("W")
+player1.move("NE")
+player1.move("NE")
+player1.move("SW")
+player1.move("S")
+player1.move("N")
+player1.move("SE") 
+print(separator)
+print("Map 1 Line 6:", ([obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+        [obj for obj in player1.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #Expected to be in (2,0)
+player1.move("S") #Expected to not change. (checking bounds)
+print(separator)
+print("Map 1 Line 7:", ([obj for obj in map1.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][0], 
+        [obj for obj in map1.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player1.id][0][1])) #Expected to be in (2,0)
+
+player1.drop("Freezer")
+print("Map 1 Line 8:", player1) #Freezer dropped, freezed for a time interval, since we didn't move away before it gets activated.
+print(separator)
+
+player2.move("E")
+print("Map 2 Line 5:", "Global View Update", ([obj for obj in player2.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][0], 
+        [obj for obj in player2.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the global map has been affected.
+print("Map 2 Line 6:", "Local View Update", ([obj for obj in player2.map.teams[player2.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][0], 
+        [obj for obj in player2.map.teams[player2.team].objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][1])) #By looking to this line we can see the player moved as it supposed to and the local map has been affected.
+player2.move("N")
+player2.move("NW")
+player2.move("NE")
+player2.move("E")
+player2.move("S")
+player2.move("SE")
+player2.move("SW") 
+print(separator)
+print("Map 2 Line 6:", ([obj for obj in player2.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][0], 
+        [obj for obj in player2.map.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][1])) #Expected to be in (2,0)
+player2.move("S") #Expected to not change. (checking bounds)
+print(separator)
+print("Map 2 Line 7:", ([obj for obj in map2.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][0], 
+        [obj for obj in map2.objects_list if obj[2].__class__.__name__ == "Player" and obj[2].id == player2.id][0][1])) #Expected to be in (2,0)
+
+player2.drop("Freezer")
+print("Map 2 Line 8:", player2) #Freezer dropped, freezed for a time interval, since we didn't move away before it gets activated.
 
 ##### Will create more test cases for the demo ####
-
-
 
