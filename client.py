bd0b527910d7de738cd9a07dcc22e74984934395
BDar01 <111367260+BDar01@users.noreply.github.com>
2023-12-10 20:01:27 +0300

    Implemented a TCP Client sample code

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
 import socket
+import signal
+import sys
 
 class GameClient:
     def __init__(self, host, port):
         self.server_address = (host, port)
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(self.server_address)
+        # Register the signal handler for CTRL+C
+        signal.signal(signal.SIGINT, self.signal_handler)
 
     def send_command(self, command):
         self.client_socket.send(command.encode())
@@ -14,24 +18,32 @@ class GameClient:
     def close(self):
         self.client_socket.close()
 
-# Example Usage:
+    def signal_handler(self, signal, frame):
+        print("Received Ctrl+C. Closing connection...")
+        self.close()
+        sys.exit(0)
 
 if __name__ == "__main__":
-    # Replace 'localhost' and 1423 with the actual server address and port
-    client = GameClient('localhost', 1423)
+    if len(sys.argv) != 3:
+        print('usage: ', sys.argv[0], 'host port')
+        sys.exit(-1)
+
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+
+    client = GameClient(host, port)
 
     try:
         while True:
             user_input = input("Enter command (or 'exit' to quit): ")
-            
+
             if user_input.lower() == 'exit':
                 break
-            
+
             response = client.send_command(user_input)
             print(response)
 
     except KeyboardInterrupt:
         pass
     finally:
-        # Close the client connection on exit
         client.close()
