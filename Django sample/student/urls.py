ea1c47045d8731659b7ca483346512203155af8d
BDar01 <111367260+BDar01@users.noreply.github.com>
2023-12-28 15:36:24 +0300

    Add 'Django sample' server (check its README first)

---
+++ Django sample/student/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path
+
+from . import views
+
+urlpatterns = [
+    re_path('^$', views.index, name='index'),
+    re_path('^detail/(?P<stid>[0-9]{5,})?$', views.detail, name='detail'),
+    re_path('^update/(?P<stid>[0-9]{5,})$', views.update, name='update'),
+    re_path('^add$', views.add, name='add'),
+    re_path('^register/(?P<stid>[0-9]{5,})?$', views.register, name='register'),
+    re_path('^accounts/logout/$', views.custom_logout, name='custom_logout'),  # Use your custom logout view
+]
